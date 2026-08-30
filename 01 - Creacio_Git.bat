20bfd32329676366eafae52ea826d2d8d317044f
jordiginjoler-coder <jordiginjoler@gmail.com>
2026-08-30 04:43:55 +0200

    feat: Tesla-style Tic-Tac-Toe with React + TypeScript + Vite

--- 01 - Creacio_Git.bat
+++ 01 - Creacio_Git.bat
@@ -1,8 +1,8 @@
 
 cd C:\Users\Jordi\AppData\Roaming\open-cowork\default_working_dir\lidr-specboot-V2-3enratlla
 git init
-git add .
+git add -A
 git commit -m "feat: Tesla-style Tic-Tac-Toe with React + TypeScript + Vite"
 git branch -M main
-git remote add origin https://github.com/jordiginjoler-coder/tictactoe.git
+git remote add origin https://github.com/jordiginjoler-coder/lidr-specboot-V2-3enratlla.git
 git push -u origin main
